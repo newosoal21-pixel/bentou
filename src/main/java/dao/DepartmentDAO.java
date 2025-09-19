b9be891eaf9f420d49435f3fa28765ddb80dd40a
6C09 <6C09@DESKTOP-9PAMI5D>
2025-09-19 12:29:08 +0900

    DepartmentDAOを更新しました。

--- src/main/java/dao/DepartmentDAO.java
+++ src/main/java/dao/DepartmentDAO.java
@@ -4,33 +4,29 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DepartmentDAO {
-	
-	public List<Department> findByLogin() {
-		 List<Department> empList = new ArrayList<>(); 
-		 String sql = "SELECT DEPARTMENTID, DEPARTMENTNAME WHERE DEPARTMENTID= ? AND DEPARTMENTNAME = ?";
-		 
-		 try (Connection conn = DBManager.getConnection();   
-         PreparedStatement pStmt = conn.prepareStatement(sql)) { // SQLを準備
-        	  
-	            //SELECT文を 実行して結果表を取得
-	            ResultSet rs = pStmt.executeQuery();
-	                while(rs.next()) {
-	                   String departmentid = rs.getString("DEPARTMENTID");
-	                   String departmentname = rs.getString("DEPARTMENTNAME");
-	                Department department = new Department(departmentid, departmentname);      
-	            }    	           
 
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	            return null;
-	        }
+	public String findById(int departmentId) {
+		String departmentname="";
+		String sql = "SELECT DEPARTMENT_ID, DEPARTMENT_NAME FROM DEPARTMENT WHERE DEPARTMENT_ID= ? ";
 
-	        return empList;
-	                        
-		 
+		try (Connection conn = DBManager.getConnection();
+				PreparedStatement pStmt = conn.prepareStatement(sql)) { // SQLを準備
+			pStmt.setInt(1, departmentId);
+
+			//SELECT文を 実行して結果表を取得
+			ResultSet rs = pStmt.executeQuery();
+			if (rs.next()) {
+				departmentname = rs.getString("DEPARTMENT_NAME");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return departmentname;
+
 	}
 }
