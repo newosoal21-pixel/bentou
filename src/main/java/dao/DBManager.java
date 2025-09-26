1f49cff9e46443e9c5fe6e42dbdf5e525c760e61
newos <newos@nexteps>
2025-09-27 08:21:11 +0900

    SQL　DB書き込み時のタイムゾーン設定をasia/tokyoに固定 /石田

--- src/main/java/dao/DBManager.java
+++ src/main/java/dao/DBManager.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
  */
 public class DBManager {
 	/** データベース接続URL */
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/lunchclerkDX?useSSL=false&serverTimezone=UTC";
+	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/lunchclerkDX?useSSL=false&serverTimezone=Asia/Tokyo";
 
 	/** データベース接続ユーザー */
 	private static final String DB_USER = "root";
